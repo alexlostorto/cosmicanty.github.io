c7ab22c381604be325555ada8a1ec079241d3e5e
CosmicAnty <100964536+CosmicAnty@users.noreply.github.com>
2022-07-17 16:28:45 +0100

    Add files via upload

---
+++ Contact/myScript.js
@@ -0,0 +1,41 @@
+let i = 1;
+
+document.getElementById("par").innerHTML = i;
+
+function add() {
+  i++;
+  document.getElementById("par").innerHTML = i;
+}
+
+function minus() {
+  i--;
+  document.getElementById("par").innerHTML = i;
+}
+
+function reset() {
+  i=0
+  document.getElementById("par").innerHTML = 0;
+}
+
+const messageInput = document.getElementById("messageInput");
+const jokeText = document.getElementById("joke-text");
+
+messageInput.addEventListener('keypress', (event)=> {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    jokeText.classList.toggle('active')
+  }
+})
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const projectButton = document.getElementsByClassName('project-button')[0]
+const projectLinks = document.getElementsByClassName('projects')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+projectButton.addEventListener('click', () => {
+  projectLinks.classList.toggle('active')
+})
